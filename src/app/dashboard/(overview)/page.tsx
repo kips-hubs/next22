0dd9cb15be00ef81b7e6759b1d3bf908fa4abf4f
KEjonathan <kejonathan75@gmail.com>
2024-07-01 23:18:33 +0300

    skeletons

---
+++ src/app/dashboard/(overview)/page.tsx
@@ -0,0 +1,21 @@
+import { lusitana } from "@/ui/fonts/fonts";
+import { CardsSkeleton } from "@/ui/skeletons";
+import { Suspense } from "react";
+
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <h1>Overview Soon Coming</h1>
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      </div>
+    </main>
+  );
+}
